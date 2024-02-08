87f1c4b6aab9f2ea6cae28e1a61e6dde04ee2456
Bikash Poudel <bikashpoudel4@gmail.com>
2024-02-08 21:23:32 +0100

    token done

---
+++ backend/ecom/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from userauths import views as userauth_views
+from store import views as store_views
+
+
+urlpatterns = [
+    path('user/token/', userauth_views.MyTokenObtainPairView.as_view()),
+]
--- backend/ecom/userauths/serializers.py
+++ backend/ecom/userauths/serializers.py
@@ -23,7 +23,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         return token
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True validators=[validate_password])
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
