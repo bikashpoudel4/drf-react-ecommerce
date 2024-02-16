2b3fd12e7d0eb310ca9109b60de355011a4fa401
Bikash Poudel <bikashpoudel4@gmail.com>
2024-02-16 01:17:23 +0100

    register component react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import {Routes, Route, BrowserRouter} from 'react-router-dom'
+import { useState } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-import Login from './views/auth/Login'
+import Login from "./views/auth/Login";
+import Register from "./views/auth/Register";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <BrowserRouter>
-      <Routes> 
-        <Route path='/login' element={<Login />}/>
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
